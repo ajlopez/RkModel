9a9453c4802e76fe6df682bf95b4eb13e453a900
ajlopez <webmaster@ajlopez.com>
2015-11-28 11:31:41 -0300

    Additional test, set property twice

--- test/events.js
+++ test/events.js
@@ -14,3 +14,29 @@ exports['on set simple property'] = function (test) {
     
     model.set('name', 'Adam');
 };
+
+exports['set simple property twice'] = function (test) {
+    test.async();
+    
+    var model = rkmodel();
+    var count = 0;
+    
+    model.on('set', 'name', function (newvalue, oldvalue) {
+        if (newvalue === 'Adam') {
+            test.equal(newvalue, 'Adam');
+            test.equal(oldvalue, undefined);
+        }
+        else {
+            test.equal(newvalue, 'Eve');
+            test.equal(oldvalue, 'Adam');
+        }
+        
+        count++;
+        
+        if (count === 2)
+            test.done();
+    });
+    
+    model.set('name', 'Adam');
+    model.set('name', 'Eve');
+};
